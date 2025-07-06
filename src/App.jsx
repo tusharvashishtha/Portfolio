a6baba42b953911de9c17b50e2ad32372a21bd7d
Siddharth6511 <vashishthatushar004@gmail.com>
2025-07-06 13:13:28 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Lenis from '@studio-freight/lenis';
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-800'>
+      APP
+    </div>
+  )
+}
+
+export default App
