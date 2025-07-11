2d82dcb8487a12ae02a0cf4e5817b0071c9f74a3
Siddharth6511 <vashishthatushar004@gmail.com>
2025-07-11 16:46:25 +0530

    footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,51 +1,56 @@
-import "./Contact.css";
-import React, { useEffect, useRef } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+import React from "react";
 
-gsap.registerPlugin(ScrollTrigger);
-
-function Contact() {
-  const imageRef = useRef(null);
-
-  useEffect(() => {
-    gsap.set(imageRef.current, { height: 0 });
-
-    gsap.to(imageRef.current, {
-      height: "150vh",
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: imageRef.current,
-        scroller: "body",
-        start: "top 80%",
-        end: "top top",
-        markers: true,
-        scrub: true,
-        pin: true
-      },
-    });
-  }, []);
-
+const Contact = () => {
   return (
-    <div className="h-[200vh] w-full flex flex-col justify-center items-center bg-amber-600">
-      {/* Absolute Centered Text */}
-      <div className="absolute z-10">
-        <h1 className="text-6xl uppercase text-center text-zinc-400 font-bold">
-          In a world of <br /> full noise
-        </h1>
+    <div className="flex h-fit w-full flex-col items-center overflow-hidden bg-black">
+        <div className="h-fit w-full flex items-center justify-center font-bold m-10 ">
+            <h1 className = "text-8xl font-[font1] uppercase text-zinc-400">Let's Connect</h1>
+        </div>
+      <div
+        className="group relative inline-flex h-16 w-40 items-center
+                   justify-center rounded-full
+                   border border-zinc-400 cursor-pointer"
+      >
+
+        <span
+          className="absolute inset-0 bg-zinc-400 transform scale-0
+                     group-hover:scale-110 transition-transform duration-400
+                     ease-out rounded-full"
+        />
+    
+        <span
+          className="relative z-10 font-[font1] text-2xl text-zinc-300
+                     group-hover:text-black transition-colors duration-300"
+        >
+          Start Project
+        </span>
+      </div>
+      <div id="navbarIconsContainer" className="h-fit p-10  border-b-1 border-zinc-400 w-full justify-evenly flex gap-4">
+        <div className="navbar2ndIcons group relative h-13 w-20 sm:w-28 sm:h-10 rounded-full border border-zinc-400 flex items-center justify-center cursor-pointer">
+          <span className="absolute inset-0 bg-zinc-400 scale-0 group-hover:scale-120 transition-transform duration-600 origin-center rounded-full"></span>
+          <i className="ri-instagram-line relative text-zinc-300 group-hover:text-black duration-600 text-2xl z-10"></i>
+        </div>
+        <div className="navbar2ndIcons group relative h-13 w-20 sm:w-30 sm:h-10 rounded-full border border-zinc-400 flex items-center justify-center cursor-pointer">
+          <span className="absolute inset-0 bg-zinc-400 scale-0 group-hover:scale-120 transition-transform duration-600 origin-center rounded-full"></span>
+          <i className="ri-github-fill relative text-zinc-300 group-hover:text-black duration-600 text-2xl z-10"></i>
+        </div>
+        <div className="navbar2ndIcons group relative  h-13 w-20 sm:w-30 sm:h-10 rounded-full border border-zinc-400 flex items-center justify-center cursor-pointer">
+          <span className="absolute inset-0 bg-zinc-400 scale-0 group-hover:scale-120 transition-transform duration-600 origin-center rounded-full"></span>
+          <i className="ri-twitter-x-fill relative text-zinc-300 group-hover:text-black duration-600 text-2xl z-10"></i>
+        </div>
+        <div className="navbar2ndIcons group relative h-13 w-20 sm:w-30 sm:h-10 rounded-full border border-zinc-400 flex items-center justify-center cursor-pointer">
+          <span className="absolute inset-0 bg-zinc-400 scale-0 group-hover:scale-120 transition-transform duration-600 origin-center rounded-full"></span>
+          <i className="ri-linkedin-box-fill relative text-zinc-300 group-hover:text-black duration-600 text-2xl z-10"></i>
+        </div>
       </div>
 
-      {/* Masked Image */}
-      <div className="contact2seconddev">
-        <img
-          ref={imageRef}
-          className="masked-image"
-          src="/mask.png"
-          alt="background"
-        />
+      <div className= "h-fit w-full flex items-center pl-8 justify-start">
+        <h1 className= "text-[18rem] text-zinc-600  font-[font7]">
+            TUSHAR<span className="text-[5rem] fonnt-light text-zinc-400 font-[font6] italic">VASHISHTHA</span> 
+        </h1>
       </div>
     </div>
   );
-}
+};
 
 export default Contact;
